f557e65ecf1ac331554813b4be5e56cce0848eb4
Krzysztof Stasiak <drwalu321@gmail.com>
2018-10-17 14:30:32 +0200

    quadrangle complete

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import numpy as np
 import rectangles
 import triangle
+import quadrangle
 
 square2 = rectangles.Square(2)
 
@@ -15,3 +16,9 @@ vec2 = np.array([4, 0])
 tr1 = triangle.Triangle(vec1, vec2)
 
 print(tr1.summary())
+
+vec3 = np.array([1, 2])
+
+qu1 = quadrangle.Quadrangle
+
+print(qu1.summary())
