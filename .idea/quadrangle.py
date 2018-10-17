ff2e674f85dc2eb144a85b23d2909fa9df9a041c
Krzysztof Stasiak <drwalu321@gmail.com>
2018-10-17 14:20:42 +0200

    quadrangle complete

--- .idea/quadrangle.py
+++ .idea/quadrangle.py
@@ -18,4 +18,13 @@ class Quadrangle(Shape):
         self.c = c
         self.d = (self.a - self.c) - self.b
         if a.shape != (2,) or b.shape != (2,) or c.shape != (2,):
-            raise ValueError('paramters have to be vectors')
+            raise ValueError('Paramters have to be vectors')
+
+    def __str__(self):
+        return 'Vectors: {}, {}, {}, {}'.format_map(self.a, self.b, self.c, self.d)
+
+    def area(self):
+        return 0.5 * abs(self.a[0] * self.b[1] - self.a[1] * self.b[0]) + 0.5 * abs((self.c[0] * self.d[1] - self.c[1] * self.d[0]))
+
+    def perimeter(self):
+        return np.linalg.norm(self.a) + np.linalg.norm(self.b) + np.linalg.norm(self.c) + np.linalg.norm(self.d)
